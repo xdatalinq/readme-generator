cd926d04c4f6f2a0030e243d8ddb87179c9a1641
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-04-15 15:18:02 -0400

    installed inquirer and begining work on questions

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = ["Please enter the a title", "Please enter a description", 
+"Please enter table of contents", "Please enter installation instructions",
+"Please enter usage instructions", "Please enter which license you would like to use",
+"Please enter contribution guidelines", "Please enter Testing instructions",
+"Please enter info for Questions section"];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
