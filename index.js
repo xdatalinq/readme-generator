a9d6e0c9b7c6e919f3fe4c97ccd5e3fbf0d1b59f
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-04-15 16:16:08 -0400

    questions section done

--- index.js
+++ index.js
@@ -3,11 +3,122 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = ["Please enter the a title", "Please enter a description", 
-"Please enter table of contents", "Please enter installation instructions",
-"Please enter usage instructions", "Please enter which license you would like to use",
-"Please enter contribution guidelines", "Please enter Testing instructions",
-"Please enter info for Questions section"];
+const promptUser = () => {
+    return inquirer.prompt([
+            {
+              type: 'input',
+              name: 'title',
+              message: "Please enter a title for your file",
+              validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a title!');
+                  return false;
+                }
+              }
+            },
+            {
+              type: 'input',
+              name: 'description',
+              message: "Please enter a description",
+              validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log("Please enter a description!");
+                  return false;
+                }
+              }
+            },
+            {
+              type: 'input',
+              name: 'tablecontents',
+              message: "Please enter table of contents",
+              validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log("Please enter table of contents!");
+                  return false;
+                }
+              }
+            },
+            {
+              type: 'input',
+              name: 'instructions',
+              message: "Please enter installation instructions",
+              validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log("Please enter installation instructions!");
+                  return false;
+                }
+              }
+            },
+            {
+              type: 'input',
+              name: 'usage',
+              message: "Please enter usage instructions",
+              validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log("Please enter usage instructions!");
+                  return false;
+                }
+              }
+            },
+            {
+                type: 'checkbox',
+                name: 'license',
+                message: "Please select which license you would like to use",
+                choices: ['MIT', 'Apache', 'GPL', 'Affero GPL', 'BSD 3-Clause', 'BSD 2-Clause', 
+                'Eclipse Public License v1.0', 'GPL v3', 'LGPL v2.1', 'LGPL v3', 
+                'Mozilla Public License Version 2.0', 'Public Domain (Unlicensed']
+            },
+            {
+              type: 'input',
+              name: 'contribution',
+              message: "Please enter contribution guidelines",
+              validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log("Please enter contribution guidelines!");
+                  return false;
+                }
+              }
+            },
+            {
+              type: 'input',
+              name: 'testing',
+              message: "Please enter Testing instructions",
+              validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log("Please enter Testing instructions!");
+                  return false;
+                }
+              }
+            },
+            {
+              type: 'input',
+              name: 'questions',
+              message: "Please enter info for Questions section",
+              validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log("Please enter info for Questions section!");
+                  return false;
+                }
+              }
+            }
+        ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
